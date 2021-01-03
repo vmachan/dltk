0bb66b1f62668a46f202e2bb501709529d6a5a51
Vinayak D. Machan <vmachan@yahoo.com>
2021-01-03 14:40:06 -0500

    Added Glue crawlers

--- cdkapp/app.py
+++ cdkapp/app.py
@@ -37,6 +37,7 @@ class sttm_record:
             ,target_format
             ,target_delimiter
             ,source_sample_datafile
+            ,target_database
             ):
         self.source_name = source_name
         self.source_location = source_location
@@ -47,12 +48,21 @@ class sttm_record:
         self.target_format = target_format
         self.target_delimiter = target_delimiter
         self.source_sample_datafile = _SAMPLE_DATA_DIR + "/" + source_sample_datafile
+        self.target_database = target_database
 
 def parse_metadata_from_excel(sttm_file_name):
     print(f"Parsing meta data from {sttm_file_name}..")
     sttm_map = []
+    _TARGET_DATABASE = ""
 
     wb = xlrd.open_workbook(sttm_file_name)
+
+    src2tgt_global_conf = wb.sheet_by_name("Framework Configuration")
+    if (src2tgt_global_conf):
+        for row in range(0, src2tgt_global_conf.nrows): # 
+            if (src2tgt_global_conf.cell_value(row, 0) == "TARGET DATABASE"):
+                _TARGET_DATABASE = src2tgt_global_conf.cell_value(row, 1)
+
     src2tgt_map = wb.sheet_by_name("Source-to-Target Mapping")
     if (src2tgt_map):
         for row in range(1, src2tgt_map.nrows): # Skipping the 1st header row - subscript 0
@@ -66,6 +76,7 @@ def parse_metadata_from_excel(sttm_file_name):
             _target_format = src2tgt_map.cell_value(row, TARGET_FORMAT_POS)
             _target_delimiter = src2tgt_map.cell_value(row, TARGET_DELIMITER_POS)
             _source_sample_datafile = src2tgt_map.cell_value(row, SOURCE_SAMPLE_DATAFILE_POS)
+            _target_database = _TARGET_DATABASE
             sttm_map.append(
                             sttm_record(
                                          _source_name
@@ -77,6 +88,7 @@ def parse_metadata_from_excel(sttm_file_name):
                                         ,_target_format
                                         ,_target_delimiter
                                         ,_source_sample_datafile
+                                        ,_TARGET_DATABASE
                                        )
                            )
     return sttm_map
@@ -85,11 +97,9 @@ def parse_metadata_from_excel(sttm_file_name):
 # Main
 #
 argparser = argparse.ArgumentParser()
-# argparser.add_argument("metadatafilename", type=str, help="Full path name of Excel file holding metadata")
 argparser.add_argument("apphomedir", type=str, help="Full path name of CDK aplication home directory")
 args = argparser.parse_args()
 _APP_HOME_DIR = args.apphomedir 
-# metadata_file_name = args.metadatafilename 
 metadata_file_name = _META_DATA_DIR + "/" + _META_DATA_FILENAME
 
 sttm_map = parse_metadata_from_excel(metadata_file_name)
@@ -98,29 +108,19 @@ curr_ACCOUNT = "424767260197"
 curr_REGION = "us-east-1"
 curr_ENV = core.Environment(account = curr_ACCOUNT, region = curr_REGION)
 
+print(args)
+
 app = core.App()
 
-frmwrkStack(app, "frmwrkS3", deploy_step=1, sttm_map=sttm_map)
+# frmwrkStack(app, "frmwrkCreateS3Buckets", deploy_step="CreateS3Buckets", sttm_map=sttm_map)
+# 
+# frmwrkStack(app, "frmwrkCopySampleDataFiles", deploy_step="CopySampleDataFiles", sttm_map=sttm_map)
+# 
+frmwrkStack(app, "frmwrkCreateGlueComponents", deploy_step="CreateGlueComponents", sttm_map=sttm_map)
 
-frmwrkStack(app, "frmwrkNEXT", deploy_step=2, sttm_map=sttm_map)
-
+# frmwrkStack(app, "frmwrkCreateS3BucketHandlers", deploy_step="CreateS3BucketHandlers", sttm_map=sttm_map)
+# 
+# frmwrkStack(app, "frmwrkNEXT", deploy_step="NEXT", sttm_map=sttm_map)
+# 
 app.synth()
-
-# Place test data files 
-def put_sample_datafile(sample_file, s3bucket):
-    print(f"In put_sample_datafile: sample_file is {sample_file}, s3bucket is {s3bucket}")
-    s3client = boto3.client('s3')
-    try:
-        response = s3client.upload_file(sample_file, s3bucket, sample_file)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
-
-for sttm_record in sttm_map:
-    if (sttm_record.source_location != "NULL"):
-        print(f"put_sample_datafile({sttm_record.source_sample_datafile}, {sttm_record.source_location})")
-        # put_sample_datafile(sttm_record.source_sample_datafile, sttm_record.source_location)
-
-# Now run stack for creating crawlers based off of the sample/test data files 
 
--- cdkapp/frmwrk/frmwrk_stack.py
+++ cdkapp/frmwrk/frmwrk_stack.py
@@ -6,10 +6,15 @@ from aws_cdk import (
         aws_glue as _glue,
         aws_iam as _iam,
         )
+import boto3
+import botocore
+from botocore.exceptions import ClientError
+import logging
+import os
 
 def print_kwargs(sttm_map_holder):
     print(f' Kwargs: {sttm_map_holder}')
-    for sttm_record in sttm_map_holder["sttm_map"]:
+    for sttm_record in sttm_map_holder:
         print(sttm_record.source_name)
         print(sttm_record.source_location)
         print(sttm_record.source_format)
@@ -19,10 +24,9 @@ def print_kwargs(sttm_map_holder):
         print(sttm_record.target_format)
         print(sttm_record.target_delimiter)
         print(sttm_record.source_sample_datafile)
+        print(sttm_record.target_database)
 
 def deploy_s3_components(currStack, sttm_map_holder):
-    # print(f' Kwargs: {sttm_map_holder}')
-    # for sttm_record in sttm_map_holder["sttm_map"]:
     for sttm_record in sttm_map_holder:
         if (sttm_record.source_location != "NULL"):
             bucket = _s3.Bucket(currStack
@@ -30,12 +34,49 @@ def deploy_s3_components(currStack, sttm_map_holder):
                     ,bucket_name = sttm_record.source_location
                     # ,versioned = true
                     ,removal_policy = core.RemovalPolicy.DESTROY # Not recommended for production
+                    # ,autoDeleteObjects = True # Not recommended for production
                     )
-            print(bucket.to_string())
+            # print(bucket.to_string())
+
+def deploy_glue_crawlers(currStack, sttm_map_holder):
+    glue_role = _iam.Role(
+                            currStack
+                          , 'glue_role'
+                          , role_name = 'frmwrkCreateGlueComponents_glue_role'
+                          , assumed_by = _iam.ServicePrincipal('glue.amazonaws.com')
+                )
+
+    glue_role.add_managed_policy(_iam.ManagedPolicy.from_aws_managed_policy_name('service-role/AWSGlueServiceRole'))
+    glue_role.add_managed_policy(_iam.ManagedPolicy.from_aws_managed_policy_name('AmazonS3FullAccess'))
+    # 'arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole'
+
+    for sttm_record in sttm_map_holder:
+        print(f'database_name is {sttm_record.target_database} for glue crawler for {sttm_record.source_location} and s3Target is {sttm_record.target_location}')
+        if (sttm_record.source_location != "NULL"):
+            crawler = _glue.CfnCrawler(currStack
+                    ,"frmwrkStack_glue_" + sttm_record.target_location
+                    ,name = "frmwrkStack_glue_" + sttm_record.target_location
+                    ,database_name = sttm_record.target_database
+                    ,role = glue_role.role_arn
+                    ,targets = {"s3Targets" : [{"path": f'{sttm_record.target_location}/'}]}
+                    ,description = "Create crawler for " + sttm_record.source_location
+                    )
+            # print(bucket.to_string())
+
+# Place test data files
+def put_sample_datafile(sample_file, s3bucket, sample_file_basename):
+    print(f"In put_sample_datafile: sample_file is {sample_file}, s3bucket is {s3bucket}, sample_file_basename is {sample_file_basename}")
+    s3client = boto3.client('s3')
+    try:
+        response = s3client.upload_file(sample_file, s3bucket, sample_file_basename)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
 
 class frmwrkStack(core.Stack):
 
-    def __init__(self, scope: core.Construct, construct_id: str, deploy_step=1, sttm_map=None, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, construct_id: str, deploy_step=None, sttm_map=None, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
         # super().__init__(scope, construct_id)
 
@@ -44,9 +85,32 @@ class frmwrkStack(core.Stack):
         # Print the **kwargs
         # print_kwargs(kwargs)
 
-        if (deploy_step == 1):
+        print(f"deploy_step is {deploy_step}")
+
+        if (deploy_step == None):
+            print("Nothing to do.. exiting..")
+
+        elif (deploy_step == "CreateS3Buckets"):
             # deploy_s3_components(self, kwargs)
             deploy_s3_components(self, sttm_map)
+
+        elif (deploy_step == "CopySampleDataFiles"):
+            print("Copying Sample data files to S3 buckets..")
+            for sttm_record in sttm_map:
+                if (sttm_record.source_location != "NULL"):
+                    datafilebasename = os.path.basename(sttm_record.source_sample_datafile)
+                    # print(f"put_sample_datafile({sttm_record.source_sample_datafile}, {sttm_record.source_location}, {datafilebasename})")
+                    put_sample_datafile(sttm_record.source_sample_datafile, sttm_record.source_location, datafilebasename)
+
+        elif (deploy_step == "CreateGlueComponents"):
+            print("Creating Glue Database first.. ")
+            _glue.Database(self, "findb", database_name = "findb")
+            print("Creating Glue components.. Crawlers, Tables, and Jobs..")
+            deploy_glue_crawlers(self, sttm_map)
+
+        elif (deploy_step == "CreateS3BucketHandlers"):
+            print("Creating event handlers on S3 buckets to handle new files when receieved..")
+
         else:
-            print("******* DEPLOY_STEP NOT 1 *******")
+            print("******* INVALID DEPLOY STEP *******")
 
